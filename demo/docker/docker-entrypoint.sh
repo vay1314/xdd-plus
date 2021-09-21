de2d3e1860b8670d7a4a7948d5a4ab6946795bac
VAY冬冬 <niuniuhaogou@gmail.com>
2021-09-21 16:19:20 +0800

    test

--- demo/docker/docker-entrypoint.sh
+++ demo/docker/docker-entrypoint.sh
@@ -6,13 +6,13 @@ dir_shell=/xdd/shell
 
 echo -e "======================1. 检测配置文件========================\n"
 fix_config
-echo
+echo -e "====================== 检测配置文件完毕 ========================\n"
 
 echo -e "======================2. 启动 XDD-plus ======================\n"
 if [[ $ENABLE_WEB_XDD == true ]]; then
         pm2 start XDD-plus
-        echo -e "XDD-plus面板启动成功...\n"
-        echo -e "访问5701端口，即可登陆面板管理."
+        echo -e " XDD-plus 面板启动完成...\n"
+        echo -e "访问5701端口，即可登陆面板管理\n"
 elif [[ $ENABLE_WEB_XDD == false ]]; then
     echo -e "已设置为不自动启动XDD-plus面板...\n"
     echo -e "默认首次不启动 XDD 面板，请编辑好配置文件后，修改环境变量为true启动面板\n"
